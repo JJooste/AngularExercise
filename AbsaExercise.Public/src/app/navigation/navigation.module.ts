14158fb212a491d95989f27de8947f63b5189454
Johan Jooste <johan.jooste@entelect.co.za>
2017-04-11 13:33:16 +0200

    Added footer component

--- AbsaExercise.Public/src/app/navigation/navigation.module.ts
+++ AbsaExercise.Public/src/app/navigation/navigation.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { AuthService } from '../shared/services/auth.service';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TopNavComponent],
-  exports: [TopNavComponent],
+  declarations: [TopNavComponent, FooterComponent],
+  exports: [TopNavComponent, FooterComponent],
   providers: [AuthService]
 })
 export class NavigationModule { }
